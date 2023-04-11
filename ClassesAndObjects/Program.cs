eb6b4e7f7a470e505f58c138bb9d9a8615530c79
Sushil <skshinde2028@gmail.com>
2023-04-11 15:44:24 +0530

    [Sushil] uploading class files

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the classes concept");
+            Car car = new Car();
+            Console.WriteLine(car.color);
+        }
+    }
+}
